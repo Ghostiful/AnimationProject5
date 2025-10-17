9f816747f34b520b17f69f142408c0053d8ff668
Daniel S. Buckstein <dbuckstein@users.noreply.github.com>
2025-10-17 18:47:36 -0400

    Update git ignore, create VS solution with main projects and files.

---
+++ cat/source/cat/cat.c
@@ -0,0 +1,30 @@
+////////////////////////////////////////////////////////////////////////////////
+/// Copyright 2025 Daniel S. Buckstein
+/// 
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+/// 
+/// http ://www.apache.org/licenses/LICENSE-2.0
+/// 
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+////////////////////////////////////////////////////////////////////////////////
+
+/*
+* cat.c
+* Main test file.
+*/
+
+#include "cat/cat.h"
+
+
+int cat_test_all(int const argc, char const* const argv[])
+{
+	(void)argc;
+	(void)argv;
+	return 0;
+}
